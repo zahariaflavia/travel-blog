201f852b22890cfd10e1fc1f61d224edd977fab2
zahariaflavia <flavia.zaharia@3pillarglobal.com>
2021-08-06 10:08:03 +0300

    footer added

--- src/pages/destinations.js
+++ src/pages/destinations.js
@@ -10,18 +10,20 @@ const Destinations = ({ data }) => {
       <Helmet>
         <title>Destinations</title>
       </Helmet>
-      <Layout >
-        <main >
-          <Article  header="Check out our latest trips" data={data}></Article>
-          <div>
-            <Contact
-              header="Interested in sharing your personal experience?"
-              linkText="Find out More"
-            >
-              These are the latest trips that you definitely want to book
-            </Contact>
+      <Layout>
+        <div>
+          <main >
+            <Article header="Check out our latest trips" data={data}></Article>
+            <div>
+              <Contact
+                header="Interested in sharing your personal experience?"
+                linkText="Find out More"
+              >
+                These are the latest trips that you definitely want to book
+              </Contact>
             </div>
-        </main>
+          </main>
+        </div>
       </Layout>
     </div>
   );
