4dfa2b7f4cb48b4042b6a7c07ba31af7f8d3c54a
zahariaflavia <flavia.zaharia@3pillarglobal.com>
2021-08-11 14:21:10 +0300

    article data and images from contentstack

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,35 +1,52 @@
 import * as React from "react";
 import Layout from "../pages/layout";
 import { graphql } from "gatsby";
-// import { MDXRenderer } from "gatsby-plugin-mdx";
-import { article } from "../pages/layout.module.css";
+import {
+  article,
+  articleImage,
+  articleContainer,
+  articleChildren,
+  imageHome
+} from "../pages/layout.module.css";
 
-const BlogPost = ({data}) => {
-    
-    console.log(data)
+const BlogPost = ({ data }) => {
   return (
     <Layout pageTitle="Articles">
       <div className={article}>
-        <div>
-          <h1>{data.contentstackBlogEntry.title}</h1>
-          <h3>
-            {data.contentstackBlogEntry.date}|{data.contentstackBlogEntry.author}
-          </h3>
-          <p>{data.contentstackBlogEntry.content}</p>
+        <h1>{data.contentstackBlogEntry.title}</h1>
+        <h3>
+          {data.contentstackBlogEntry.date}|{data.contentstackBlogEntry.author}
+        </h3>
+
+        <div className={articleContainer}>
+          <div className={articleChildren}>
+            <p>{data.contentstackBlogEntry.content}</p>
+          </div>
+          <div className={articleChildren}>
+            <img
+            className={imageHome}
+              src={data.contentstackBlogEntry.image.url}
+              alt="img"
+            />
+          </div>
         </div>
       </div>
     </Layout>
   );
 };
 export default BlogPost;
-export const postQuery =graphql`
-query MyQuery($title:String){
-    contentstackBlogEntry(title: { eq: $title }){
+export const postQuery = graphql`
+  query MyQuery($title: String) {
+    contentstackBlogEntry(title: { eq: $title }) {
+      url
+      title
+      date(formatString: "MMMM D, YYYY")
+      content
+      author
+      image {
+        id
         url
-        title
-        date(formatString: "MMMM D, YYYY")
-        content
-        author
+      }
     }
-}
-`
+  }
+`;
