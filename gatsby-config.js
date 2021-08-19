fbadf68b5ecd6aaa2cacc577dd8eed6ccf2875fc
zahariaflavia <flavia.zaharia@3pillarglobal.com>
2021-08-19 11:31:11 +0300

    added react markdown for content

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-transformer-remark",
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-source-filesystem",
